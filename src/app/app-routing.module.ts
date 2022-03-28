47798a61a4f50db6b13c1b3af4708ae61293541e
venkatprasad-CGVAK <venkatprasad@cgvakindia.com>
2022-03-28 11:33:23 +0530

    Login page created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './common/about-page/about-page.component';
+import { HomePageComponent } from './common/home-page/home-page.component';
+import { LoginFormComponent } from './common/login-form/login-form.component';
+import { SigninFormComponent } from './common/signin-form/signin-form.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"login",component:LoginFormComponent},
+  {path:"signin",component:SigninFormComponent},
+  {path:"about",component:AboutPageComponent},
+  {path:"home",component:HomePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutPageComponent } from './common/about-page/about-page.component';
+import { HomePageComponent } from './common/home-page/home-page.component';
+import { LoginFormComponent } from './common/login-form/login-form.component';
+import { NavBarComponent } from './common/nav-bar/nav-bar.component';
+import { SideMenuComponent } from './common/side-menu/side-menu.component';
+import { SigninFormComponent } from './common/signin-form/signin-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SideMenuComponent,
+    LoginFormComponent,
+    SigninFormComponent,
+    HomePageComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule,
